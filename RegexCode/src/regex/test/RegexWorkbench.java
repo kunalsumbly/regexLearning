2b650dcd27b2186b2b6eeda795459ae6df66a7cc
kusu <kunal.sumbly@sofico.com.au>
2018-10-25 09:37:48 +1100

    Code refactoring with Junits

---
+++ RegexCode/src/regex/test/RegexWorkbench.java
@@ -0,0 +1,40 @@
+package regex.test;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexWorkbench {
+	
+	public int stringWithEscapeCharacter(String [] inputString) {
+		/**
+		 * \ is represented by \\ in java 
+		 * so to actually match "\" in a string using regex we first need to escape the meta character "\" itself using "\\"
+		 * so in java "\\" becomes "\\\\" --> brain fart
+		 */
+		try{
+			Pattern pattern = Pattern.compile("\\\\"); 
+			runPattern(pattern,inputString);
+			return countNumberOfMatches(pattern, inputString);
+		}catch (Exception e) {
+			return 0;
+		}
+	}
+
+
+	private void runPattern(Pattern pattern,String [] inputString) {
+		for (String str : inputString) {
+			System.out.println("String to be matched::::"+str);
+			Matcher matcher = pattern.matcher(str);
+			System.out.println("Found the match??"+matcher.find());
+		}
+	}
+	
+	private int countNumberOfMatches(Pattern pattern,String [] inputString) {
+		int count=0;
+		for (String str : inputString) {
+			Matcher matcher = pattern.matcher(str);
+			count=matcher.find()?++count:count;
+		}
+		return count;
+	}
+
+}
---
+++ RegexCode/src/regex/test/RegexWorkbenchJunit.java
@@ -0,0 +1,16 @@
+package regex.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RegexWorkbenchJunit {
+	private RegexWorkbench targetObj = new RegexWorkbench();
+
+	@Test
+	public void teststringWithEscapeCharacter() {
+		String[] s1 = { "sales1.xls", "orders3.xls", "sales2.xls", "sales3.xls", "apac1.xls", "europe2.xls", "na\\.xls",
+				"na2.xls", "sa1.xls", "sa1.doc", "sales.xls","ca1.xls" };
+		Assert.assertEquals(1,targetObj.stringWithEscapeCharacter(s1)); // this pattern should only match na\\.xls string
+	}
+
+}
