2aeb13a83e1ae10ba8bdcb2ca8dba0f87f62f8d9
kunalsumbly <kunal.sumbly@gmail.com>
2018-10-26 11:10:52 +1100

    Update RegexWorkbenchJunit.java

--- RegexCode/src/regex/test/RegexWorkbenchJunit.java
+++ RegexCode/src/regex/test/RegexWorkbenchJunit.java
@@ -17,7 +17,7 @@ public class RegexWorkbenchJunit {
 	public void teststringWithSpecificCharacterSet() {
 		String[] s1 = { "sales1.xls", "orders3.xls", "sales2.xls", "sales3.xls", "apac1.xls", "europe2.xls", "na\\.xls",
 				"na2.xls", "sa1.xls", "sa1.doc", "sales.xls","ca1.xls" };
-		Assert.assertEquals(3,targetObj.stringWithSpecificCharacterSet(s1)); // this pattern should only match na2.xls, sa1.xls
+		Assert.assertEquals(3,targetObj.stringWithSpecificCharacterSet(s1)); // this pattern should only match na\.xls , na2.xls, sa1.xls
 	}
 
 }
